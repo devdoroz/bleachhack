42dde66b6dd1ab24177bfddfe758b2b1f2f60113
doroz <125635258+devdoroz@users.noreply.github.com>
2024-06-13 21:37:11 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,6 +3,7 @@ local debris = game:GetService("Debris")
 
 local games = {
 	[4777817887] = "https://api.luarmor.net/files/v3/loaders/86d5ae1b35228252db1cd70ae526ede9.lua",
+	[3150475059] = "https://api.luarmor.net/files/v3/loaders/abe9928b4d6cef86792a8144f8037d73.lua",
 }
 
 local link = games[game.GameId]
