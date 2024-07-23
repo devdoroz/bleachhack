b104777803a71902eb0162b0e6c6870a0df80edf
doroz <125635258+devdoroz@users.noreply.github.com>
2024-07-22 21:10:24 -0700

    Update newnewloader.lua

--- newnewloader.lua
+++ newnewloader.lua
@@ -3,13 +3,13 @@ local debris = game:GetService("Debris")
 
 local games = {
 	[4777817887] = "https://api.luarmor.net/files/v3/loaders/3915a1fdfb05a5f46118eb6fccbd48a6.lua",
-	[3150475059] = "https://api.luarmor.net/files/v3/loaders/7e2acc0ac5385e8677fcfdf16efcd510.lua",
+	[3150475059] = "https://api.luarmor.net/files/v3/loaders/50ac53ef9bc082c5e184696d281fc839.lua",
 	[184199275] = "https://api.luarmor.net/files/v3/loaders/ae959005deeaceec2423cacc3d5ff772.lua"
 }
 
 local freeGames = {
 	[4777817887] = "https://api.luarmor.net/files/v3/loaders/3915a1fdfb05a5f46118eb6fccbd48a6.lua",
-	[3150475059] = "https://api.luarmor.net/files/v3/loaders/50ac53ef9bc082c5e184696d281fc839.lua",
+	[3150475059] = "https://api.luarmor.net/files/v3/loaders/7e2acc0ac5385e8677fcfdf16efcd510.lua",
 	[184199275] = "https://api.luarmor.net/files/v3/loaders/ae959005deeaceec2423cacc3d5ff772.lua"
 }
 
